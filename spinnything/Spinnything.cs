3373f0228cbeeee23c289cc545fdcd7acada2af2
Rat <mettaur@yahoo.com>
2015-11-16 00:57:22 -0500

    Init
    Yes.

---
+++ spinnything/Spinnything.cs
@@ -0,0 +1,27 @@
+﻿/* SPINNYTHING :3
+ * Made by Rat
+ * Version 1.0 FINAL.
+ */
+
+using System;
+using System.Text;
+
+namespace consolelog
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("Loading... ");
+			while (true) 
+			{
+				Console.Write ("/");
+				Console.Write ("\b");
+				Console.Write ("-");
+				Console.Write ("\b");
+				Console.Write ("\\");
+				Console.Write ("\b");
+			}
+		}
+	}
+}
